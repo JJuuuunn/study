7783f4f7c5436048f5852412d25d2becad7e877a
jjuuuunn <didtdjwns111@gmail.com>
2023-02-28 01:37:42 +0900

    주문상품 전체 가격 조회 로직 추가

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -33,4 +33,12 @@ public class OrderItem {
     public void cancel() {
         this.getItem().addStock(count);
     }
+
+    //===조회 로직===//
+    /**
+     * 주문상품 전체 가격 조회
+     */
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
