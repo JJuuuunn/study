83ee1fc3b07b056b5b0d95706541cb6cb5cda90e
jjuuuunn <didtdjwns111@gmail.com>
2023-03-02 19:14:33 +0900

    Order 컬렉션 리스트를 fetch join 으로 조회하는 쿼리문 추가

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -104,10 +104,21 @@ public class OrderRepository {
 
     public List<OrderSimpleQueryDto> findOrderDtos() {
         return em.createQuery(
-                "select new jpabook.jpashop.repository.OrderSimpleQueryDto(o.id, m.name, o.orderDate, o.status, d.address) " +
+                        "select new jpabook.jpashop.repository.OrderSimpleQueryDto(o.id, m.name, o.orderDate, o.status, d.address) " +
+                                "from Order o " +
+                                "join o.member m " +
+                                "join o.delivery d", OrderSimpleQueryDto.class)
+                .getResultList();
+    }
+
+    public List<Order> findAllWithItem() {
+        return em.createQuery(
+                "select distinct o " +
                         "from Order o " +
-                        "join o.member m " +
-                        "join o.delivery d", OrderSimpleQueryDto.class)
-        .getResultList();
+                        "join fetch o.member m " +
+                        "join fetch o.delivery d " +
+                        "join fetch o.orderItems oi " +
+                        "join fetch oi.item i", Order.class)
+                .getResultList();
     }
 }
