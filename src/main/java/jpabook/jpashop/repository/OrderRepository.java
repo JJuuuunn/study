ea9773a76cf37490451715ba5f94b6bb23c59fd0
jjuuuunn <didtdjwns111@gmail.com>
2023-03-03 18:00:01 +0900

    findOrderDtos() 메소드 저장 위치 변경

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -102,15 +102,6 @@ public class OrderRepository {
                 .getResultList();
     }
 
-    public List<OrderSimpleQueryDto> findOrderDtos() {
-        return em.createQuery(
-                        "select new jpabook.jpashop.repository.OrderSimpleQueryDto(o.id, m.name, o.orderDate, o.status, d.address) " +
-                                "from Order o " +
-                                "join o.member m " +
-                                "join o.delivery d", OrderSimpleQueryDto.class)
-                .getResultList();
-    }
-
     public List<Order> findAllWithItem() {
         return em.createQuery(
                 "select distinct o " +
