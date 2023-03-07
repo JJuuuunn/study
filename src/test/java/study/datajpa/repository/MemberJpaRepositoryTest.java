cfc024d4c5f53168134d3e0e53cdb514e260af04
jjuuuunn <didtdjwns111@gmail.com>
2023-03-07 14:08:54 +0900

    벌크성 수 테스트

--- src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -119,6 +119,21 @@ class MemberJpaRepositoryTest {
         //then
         assertThat(members.size()).isEqualTo(3);
         assertThat(totalCount).isEqualTo(5);
+    }
 
+    @Test
+    public void bulkUpdate() {
+        //given
+        memberJpaRepository.save(new Member("member1", 10));
+        memberJpaRepository.save(new Member("member2", 15));
+        memberJpaRepository.save(new Member("member3", 20));
+        memberJpaRepository.save(new Member("member4", 25));
+        memberJpaRepository.save(new Member("member5", 30));
+
+        //when
+        int resultCount = memberJpaRepository.bulkAgePlus(20);
+
+        //then
+        assertThat(resultCount).isEqualTo(3);
     }
 }
