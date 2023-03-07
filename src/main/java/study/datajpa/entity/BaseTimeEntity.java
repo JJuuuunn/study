ff13da0e8f58749aa2bda48bdb0285004b568c32
jjuuuunn <didtdjwns111@gmail.com>
2023-03-07 20:57:16 +0900

    (등록자, 수정자) 와 (등록시간, 수정시간) 분리

--- src/main/java/study/datajpa/entity/BaseTimeEntity.java
+++ src/main/java/study/datajpa/entity/BaseTimeEntity.java
@@ -7,17 +7,20 @@ import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
 @EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
 @Getter
-public class BaseEntity extends BaseTimeEntity {
+public class BaseTimeEntity {
 
-    @CreatedBy
+    @CreatedDate
     @Column(updatable = false)
-    private String createdBy;
-    @LastModifiedBy
-    private String lastModifiedBy;
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime lastModifiedDate;
 }
