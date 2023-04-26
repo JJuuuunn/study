d60dc3452128c37ebf7d5334ebe2f0161b255d8c
jjuuuunn <didtdjwns111@gmail.com>
2023-04-26 21:32:19 +0900

    Querydsl 조인 - 서브 쿼ˆ(Goe)

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -343,4 +343,25 @@ class QuerydslBasicTest {
                 .extracting("age")
                 .containsExactly(40);
     }
+
+    /**
+     * 나이가 평균 나이 이상인 회원
+     */
+    @Test
+    public void subQueryGoe() throws Exception {
+        QMember memberSub = new QMember("memberSub");
+
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .where(member.age.goe(
+                        JPAExpressions
+                                .select(memberSub.age.avg())
+                                .from(memberSub)
+                ))
+                .fetch();
+
+        assertThat(result)
+                .extracting("age")
+                .containsExactly(30, 40);
+    }
 }
