8bfc4780ffaf72a0bd46fc998ad4d6eadbe34cbe
jjuuuunn <didtdjwns111@gmail.com>
2023-05-02 16:06:43 +0900

    동적 쿼리와 성능 최적화 조회 - Where 절 파라미터 사용

--- src/test/java/study/querydsl/repository/MemberJpaRepositoryTest.java
+++ src/test/java/study/querydsl/repository/MemberJpaRepositoryTest.java
@@ -57,7 +57,7 @@ public void basicQuerydslTest() throws Exception {
     }
     
     @Test
-    public void searchTest() throws Exception {
+    public void searchByBuilderTest() throws Exception {
         //given
         Team teamA = new Team("teamA");
         em.persist(teamA);
@@ -81,6 +81,36 @@ public void basicQuerydslTest() throws Exception {
         condition.setTeamName("teamB");
 
         List<MemberTeamDto> result = memberJpaRepository.searchByBuilder(condition);
+
+        //then
+        assertThat(result).extracting("username").containsExactly("member3", "member4");
+    }
+
+    @Test
+    public void searchTest() throws Exception {
+        //given
+        Team teamA = new Team("teamA");
+        em.persist(teamA);
+        Team teamB = new Team("teamB");
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        em.persist(member1);
+        em.persist(member2);
+
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        em.persist(member3);
+        em.persist(member4);
+
+        //when
+        MemberSearchCondition condition = new MemberSearchCondition();
+//        condition.setAgeGoe(20);
+//        condition.setAgeLoe(40);
+        condition.setTeamName("teamB");
+
+        List<MemberTeamDto> result = memberJpaRepository.search(condition);
 
         //then
         assertThat(result).extracting("username").containsExactly("member3", "member4");
