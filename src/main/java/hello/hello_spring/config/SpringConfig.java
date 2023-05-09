70c61feb3061ff024a99723514c15d4a942925d7
jjuuuunn <didtdjwns111@gmail.com>
2023-05-10 01:21:27 +0900

    순수 jdbc

--- src/main/java/hello/hello_spring/config/SpringConfig.java
+++ src/main/java/hello/hello_spring/config/SpringConfig.java
@@ -1,21 +1,26 @@
 package hello.hello_spring.config;
 
 import hello.hello_spring.respository.MemberRepository;
-import hello.hello_spring.respository.MemoryMemberRepository;
+import hello.hello_spring.respository.JdbcMemberRepository;
 import hello.hello_spring.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
-
+    private final DataSource dataSource;
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
     }
-
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
