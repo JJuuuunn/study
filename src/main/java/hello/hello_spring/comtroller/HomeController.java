a3d7cf3cc2a891c971082fe3ac98d3c11df88d0d
jjuuuunn <didtdjwns111@gmail.com>
2023-05-09 21:13:53 +0900

    회원 웹 기능 - 등록

--- src/main/java/hello/hello_spring/comtroller/HomeController.java
+++ src/main/java/hello/hello_spring/comtroller/HomeController.java
@@ -1,13 +1,38 @@
 package hello.hello_spring.comtroller;
 
+import hello.hello_spring.domain.Member;
+import hello.hello_spring.service.MemberService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController {
+
+    private final MemberService memberService;
+
+    public HomeController(MemberService memberService) {
+        this.memberService = memberService;
+    }
 
     @GetMapping("/")
     public String home() {
         return "home";
     }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
 }
