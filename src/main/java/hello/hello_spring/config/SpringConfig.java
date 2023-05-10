9f0428c3195b197515fdab4c96d725c0d9a12969
jjuuuunn <didtdjwns111@gmail.com>
2023-05-10 09:18:59 +0900

    JPA

--- src/main/java/hello/hello_spring/config/SpringConfig.java
+++ src/main/java/hello/hello_spring/config/SpringConfig.java
@@ -1,28 +1,36 @@
 package hello.hello_spring.config;
 
 import hello.hello_spring.respository.JdbcTemplateMemberRepository;
+import hello.hello_spring.respository.JpaMemberRepository;
 import hello.hello_spring.respository.MemberRepository;
 import hello.hello_spring.respository.JdbcMemberRepository;
 import hello.hello_spring.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.persistence.EntityManager;
 import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
     private final DataSource dataSource;
-    public SpringConfig(DataSource dataSource) {
+    private final EntityManager em;
+
+    public SpringConfig(DataSource dataSource, EntityManager em) {
         this.dataSource = dataSource;
+        this.em = em;
     }
+
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
     }
+
     @Bean
     public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
 //        return new JdbcMemberRepository(dataSource);
-        return new JdbcTemplateMemberRepository(dataSource);
+//        return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
     }
 }
