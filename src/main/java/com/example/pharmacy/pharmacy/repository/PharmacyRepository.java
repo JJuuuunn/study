5f576e1000928e1acc9582aa0c8223540a8ea5e1
jjuuuunn <didtdjwns111@gmail.com>
2023-05-31 12:22:46 +0900

    Testcontainers 를 이용한 통합 테스트 환경 구성

---
+++ src/main/java/com/example/pharmacy/pharmacy/repository/PharmacyRepository.java
@@ -0,0 +1,10 @@
+package com.example.pharmacy.pharmacy.repository;
+
+import com.example.pharmacy.pharmacy.entity.Pharmacy;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PharmacyRepository extends JpaRepository<Pharmacy, Long> {
+
+}
