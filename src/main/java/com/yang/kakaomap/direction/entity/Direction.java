9d5d145447269c614c74bc0151daf6989aef4e7e
jjuuuunn <didtdjwns111@gmail.com>
2023-06-22 18:51:43 +0900

    Direction Entity 생성

---
+++ src/main/java/com/yang/kakaomap/direction/entity/Direction.java
@@ -0,0 +1,36 @@
+package com.yang.kakaomap.direction.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity(name = "direction")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+public class Direction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // 목적지
+    private String inputAddress;
+    private double inputLatitude;
+    private double inputLongitude;
+
+    // 주변 위치(주차장 or 약국)
+    private String targetPharmacyName;
+    private String targetAddress;
+    private double targetLatitude;
+    private double targetLongitude;
+
+    // 목적지과 주변 위치(주차장 or 약국) 간의 거리
+    private double distance;
+}
