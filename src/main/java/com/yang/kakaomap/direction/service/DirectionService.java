b764019b5bcf024d93e21460ba22a08499df4f71
jjuuuunn <didtdjwns111@gmail.com>
2023-06-22 18:58:16 +0900

    Direction Service 생성

---
+++ src/main/java/com/yang/kakaomap/direction/service/DirectionService.java
@@ -0,0 +1,13 @@
+package com.yang.kakaomap.direction.service;
+
+import com.yang.kakaomap.direction.repository.DirectionRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class DirectionService {
+
+}
