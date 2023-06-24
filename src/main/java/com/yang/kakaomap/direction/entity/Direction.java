2956e302e15023fb8728c1f8383aafa8ef1697bd
jjuuuunn <didtdjwns111@gmail.com>
2023-06-24 14:33:23 +0900

    BaseTimeEntity 상속

--- src/main/java/com/yang/kakaomap/direction/entity/Direction.java
+++ src/main/java/com/yang/kakaomap/direction/entity/Direction.java
@@ -1,5 +1,6 @@
 package com.yang.kakaomap.direction.entity;
 
+import com.yang.kakaomap.common.BaseTimeEntity;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -15,7 +16,7 @@ import javax.persistence.Id;
 @NoArgsConstructor
 @Builder
 @Getter
-public class Direction {
+public class Direction extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
