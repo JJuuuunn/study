9333aee7d52ff96805125a2077f223ffccc84a46
jjuuuunn <didtdjwns111@gmail.com>
2023-06-24 14:34:25 +0900

    Pharmacy Entity Column 수정

--- src/main/java/com/yang/kakaomap/pharmacy/entity/Pharmacy.java
+++ src/main/java/com/yang/kakaomap/pharmacy/entity/Pharmacy.java
@@ -1,18 +1,38 @@
 package com.yang.kakaomap.pharmacy.entity;
 
-import javax.persistence.*;
+import com.yang.kakaomap.common.entity.BaseTimeEntity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
-public class Pharmacy {
+public class Pharmacy extends BaseTimeEntity {
 
     @Id
-    private String hpId; // 기관 Id
+    private String id; // 기관 Id (hpId)
     private String name; // 이름
     private String info; // 상세설명
     private String tel; // 전화번호
-    private String time; // 운영시간 (dutyTime1S + " ~ " + dutyTime1C + "\n" + ・・・・・・
     private String address; // 주소
     private String zipCode; // 우편번호 (postCdn1 + postCdn2)
     private double latitude; // 위도
-    private double longitude; // 경도
+    private double longitude; // 경도일
+
+    // 요일별(+공휴일) 영엄시간 (오픈시간 ~ 마감시간)
+    private String monOpenTime;
+    private String monCloseTime;
+    private String tueOpenTime;
+    private String tueCloseTime;
+    private String wedOpenTime;
+    private String wedCloseTime;
+    private String thuOpenTime;
+    private String thuCloseTime;
+    private String friOpenTime;
+    private String friCloseTime;
+    private String satOpenTime;
+    private String satCloseTime;
+    private String sunOpenTime;
+    private String sunCloseTime;
+    private String holOpenTime;
+    private String holCloseTime;
 }
