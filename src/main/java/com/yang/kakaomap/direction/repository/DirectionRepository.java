b236d3809d5970311d01e12b8fb26d5c5fb668b7
jjuuuunn <didtdjwns111@gmail.com>
2023-06-30 22:15:59 +0900

    Repository 수정

--- src/main/java/com/yang/kakaomap/direction/repository/DirectionRepository.java
+++ src/main/java/com/yang/kakaomap/direction/repository/DirectionRepository.java
@@ -1,14 +1,8 @@
 package com.yang.kakaomap.direction.repository;
 
 import com.yang.kakaomap.direction.entity.Direction;
-import lombok.*;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
 @Repository
 public interface DirectionRepository extends JpaRepository<Direction, Long> {
