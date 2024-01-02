5aa2af3b6c466b803811c5ad8fb315ece136af7b
jjuuuunn <didtdjwns111@gmail.com>
2024-01-02 11:19:10 +0900

    FEAT: Create insertManager1 Method that operates with preparedStatement 동적쿼리를 사용하기 위해 사용 TODO: 동작 확인하였고, 추후 insertManager 메소드 대신 사용하도록 변경

--- employee/EmployeeDBIO.java
+++ employee/EmployeeDBIO.java
@@ -9,12 +9,13 @@ import lib.ObjectDBIO;
 
 public abstract class EmployeeDBIO extends ObjectDBIO implements EmployeeIO {
 
-	/**
-	 * insertEmp 메소드
-	 * @param emp
-	 * @return
-	 */
-	public boolean insertEmp(Staff emp) {
+    /**
+     * insertEmp 메소드
+     *
+     * @param emp
+     * @return
+     */
+    public boolean insertStaff1(Staff emp) {
         String insertSql = "insert into EMPLOYEE values (?, ?, ?, ?, ?, ?, null)";
         Connection conn = super.open();
         try {
@@ -42,31 +43,66 @@ public abstract class EmployeeDBIO extends ObjectDBIO implements EmployeeIO {
 		return true;
 	}
 
-	public boolean insertSecretary(Secretary emp) {
-		return insertStaff(emp);
-	}
-	
-	public boolean insertManager(Manager emp) {
-		ArrayList<Employee> resArray = searchEmployee(emp.getSecNo());
-		if(resArray.isEmpty())
-			return false;
-		
-		String strSql = "insert into EMPLOYEE values('" + emp.getENo() + "','" + emp.getName() + "'," + emp.getYear() + "," + 
-		emp.getMonth() + "," + emp.getDate() + ",'" + emp.getRole() + "','" + emp.getSecNo() + "')";
-		
-		super.execute(strSql);
-		return true;
-	}
+    public boolean insertSecretary(Secretary emp) {
+        return insertStaff(emp);
+    }
 
-	/**
-	 * getEmployeeList 메소드
-	 * @param strUserID
-	 * @return
-	 * @throws SQLException
-	 */
-	public ArrayList<Employee> selectEmpList(String strUserID) throws SQLException {
-		ArrayList<Employee> resArray = new ArrayList<Employee>();
-		String selectSql = "select * from EMPLOYEE where (role != 'Manager') or (secno = ?)";
+    /**
+     * insertManager 메소드
+     *
+     * @param emp
+     * @return
+     * @throws SQLException
+     */
+    public boolean insertManager1(Manager emp) throws SQLException {
+        ArrayList<Employee> resArray = selectEmp(emp.getSecNo());
+        if (resArray.isEmpty())
+            return false;
+
+        String insertSql = "insert into EmPLOYEE values(?,?,?,?,?,?,?)";
+
+        try {
+            Connection conn = super.open();
+            PreparedStatement pstm = conn.prepareStatement(insertSql);
+            pstm.setString(1, emp.getENo());    // eno
+            pstm.setString(2, emp.getName());   // name
+            pstm.setInt(3, emp.getYear());      // year
+            pstm.setInt(4, emp.getMonth());     // month
+            pstm.setInt(5, emp.getDate());      // date
+            pstm.setString(6, emp.getRole());   // role
+            pstm.setString(7, emp.getSecNo());  // secNo
+
+            pstm.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            super.close();
+        }
+        return true;
+    }
+
+    public boolean insertManager(Manager emp) {
+        ArrayList<Employee> resArray = searchEmployee(emp.getSecNo());
+        if (resArray.isEmpty())
+            return false;
+
+        String strSql = "insert into EMPLOYEE values('" + emp.getENo() + "','" + emp.getName() + "'," + emp.getYear() + "," +
+                emp.getMonth() + "," + emp.getDate() + ",'" + emp.getRole() + "','" + emp.getSecNo() + "')";
+
+        super.execute(strSql);
+        return true;
+    }
+
+    /**
+     * getEmployeeList 메소드
+     *
+     * @param strUserID
+     * @return
+     * @throws SQLException
+     */
+    public ArrayList<Employee> selectEmpList(String strUserID) throws SQLException {
+        ArrayList<Employee> resArray = new ArrayList<Employee>();
+        String selectSql = "select * from EMPLOYEE where (role != 'Manager') or (secno = ?)";
 
 		Connection conn = super.open();
 		PreparedStatement pstm = conn.prepareStatement(selectSql);
@@ -130,15 +166,16 @@ public abstract class EmployeeDBIO extends ObjectDBIO implements EmployeeIO {
 		return resArray;
 	}
 
-	/**
-	 * searchEmployee 메소드
-	 * @param strENo
-	 * @return
-	 * @throws SQLException
-	 */
-	public ArrayList<Employee> selectEmp(String strENo) throws SQLException {
-		ArrayList<Employee> resArray = new ArrayList<Employee>();
-		String selectSql = "select * from EMPLOYEE where eno = ?";
+    /**
+     * searchEmployee 메소드
+     *
+     * @param strENo
+     * @return
+     * @throws SQLException
+     */
+    public ArrayList<Employee> selectEmp(String strENo) throws SQLException {
+        ArrayList<Employee> resArray = new ArrayList<Employee>();
+        String selectSql = "select * from EMPLOYEE where eno = ?";
 
 		Connection conn = super.open();
 		PreparedStatement pstm = conn.prepareStatement(selectSql);
