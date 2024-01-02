1299ce6f3e5ba06389d9f733016198cbf185c133
jjuuuunn <didtdjwns111@gmail.com>
2024-01-02 11:41:30 +0900

    REFACTOR : Change Method 기존의 Statement로 동작하던 메소드들을 전부 PreparedStatement로 동작할 수 있게 변경

--- employee/EmployeeDBIO.java
+++ employee/EmployeeDBIO.java
@@ -10,13 +10,14 @@ import lib.ObjectDBIO;
 public abstract class EmployeeDBIO extends ObjectDBIO implements EmployeeIO {
 
     /**
-     * insertEmp 메소드
+     * insertStaff 메소드
      *
      * @param emp
      * @return
      */
-    public boolean insertStaff1(Staff emp) {
+    public boolean insertStaff(Staff emp) {
         String insertSql = "insert into EMPLOYEE values (?, ?, ?, ?, ?, ?, null)";
+
         Connection conn = super.open();
         try {
             PreparedStatement pstm = conn.prepareStatement(insertSql);
@@ -30,18 +31,10 @@ public abstract class EmployeeDBIO extends ObjectDBIO implements EmployeeIO {
         } catch (SQLException e) {
             e.printStackTrace();
         } finally {
-            super.close();
+            close(conn);
         }
         return true;
     }
-
-	public boolean insertStaff(Staff emp) {
-		String strSql = "insert into EMPLOYEE values('" + emp.getENo() + "','" + emp.getName() + "'," + emp.getYear() + "," +
-		emp.getMonth() + "," + emp.getDate() + ",'" + emp.getRole() + "', null )";
-
-		super.execute(strSql);
-		return true;
-	}
 
     public boolean insertSecretary(Secretary emp) {
         return insertStaff(emp);
@@ -54,15 +47,14 @@ public abstract class EmployeeDBIO extends ObjectDBIO implements EmployeeIO {
      * @return
      * @throws SQLException
      */
-    public boolean insertManager1(Manager emp) throws SQLException {
-        ArrayList<Employee> resArray = selectEmp(emp.getSecNo());
-        if (resArray.isEmpty())
-            return false;
+    public boolean insertManager(Manager emp) {
+        ArrayList<Employee> resArray = searchEmployee(emp.getSecNo());
+        if (resArray.isEmpty()) return false;
 
         String insertSql = "insert into EmPLOYEE values(?,?,?,?,?,?,?)";
 
+        Connection conn = super.open();
         try {
-            Connection conn = super.open();
             PreparedStatement pstm = conn.prepareStatement(insertSql);
             pstm.setString(1, emp.getENo());    // eno
             pstm.setString(2, emp.getName());   // name
@@ -75,21 +67,10 @@ public abstract class EmployeeDBIO extends ObjectDBIO implements EmployeeIO {
             pstm.execute();
         } catch (SQLException e) {
             e.printStackTrace();
+            return false;
         } finally {
             super.close();
         }
-        return true;
-    }
-
-    public boolean insertManager(Manager emp) {
-        ArrayList<Employee> resArray = searchEmployee(emp.getSecNo());
-        if (resArray.isEmpty())
-            return false;
-
-        String strSql = "insert into EMPLOYEE values('" + emp.getENo() + "','" + emp.getName() + "'," + emp.getYear() + "," +
-                emp.getMonth() + "," + emp.getDate() + ",'" + emp.getRole() + "','" + emp.getSecNo() + "')";
-
-        super.execute(strSql);
         return true;
     }
 
@@ -100,17 +81,18 @@ public abstract class EmployeeDBIO extends ObjectDBIO implements EmployeeIO {
      * @return
      * @throws SQLException
      */
-    public ArrayList<Employee> selectEmpList(String strUserID) throws SQLException {
+    public ArrayList<Employee> getEmployeeList(String strUserID) {
         ArrayList<Employee> resArray = new ArrayList<Employee>();
+
         String selectSql = "select * from EMPLOYEE where (role != 'Manager') or (secno = ?)";
 
-		Connection conn = super.open();
-		PreparedStatement pstm = conn.prepareStatement(selectSql);
-		pstm.setString(1, strUserID);	// secno
-
+        Connection conn = super.open();
 		ResultSet rs = null;
 		try {
-			rs = pstm.executeQuery(selectSql);
+            PreparedStatement pstm = conn.prepareStatement(selectSql);
+            pstm.setString(1, strUserID);	// secno
+
+			rs = pstm.executeQuery();
 			while(rs.next()) {
 				Employee emp = null;
 				if(rs.getString("role").equals("Staff")) {
@@ -123,37 +105,6 @@ public abstract class EmployeeDBIO extends ObjectDBIO implements EmployeeIO {
 				}
 				else if(rs.getString("role").equals("Manager")) {
 					emp = new Manager(rs.getString("eno"), rs.getString("secno"), rs.getString("name"),
-							rs.getInt("enteryear"), rs.getInt("entermonth"), rs.getInt("enterday"));
-				}
-				resArray.add(emp);
-			}
-			rs.close();
-			super.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return resArray;
-	}
-
-	public ArrayList<Employee> getEmployeeList(String strUserID) {
-		ArrayList<Employee> resArray = new ArrayList<Employee>();
-		String strSql = "select * from EMPLOYEE where (role != 'Manager') or (secno = '" + strUserID + "')";
-		ResultSet rs = null;
-
-		try {
-			rs = super.execute(strSql, rs);
-			while(rs.next()) {
-				Employee emp = null;
-				if(rs.getString("role").equals("Staff")) {
-					emp = new Staff(rs.getString("eno"), rs.getString("name"), rs.getInt("enteryear"), 
-							rs.getInt("entermonth"), rs.getInt("enterday"));
-				}
-				else if(rs.getString("role").equals("Secretary")) {
-					emp = new Secretary(rs.getString("eno"), rs.getString("name"), rs.getInt("enteryear"), 
-							rs.getInt("entermonth"), rs.getInt("enterday"));
-				}
-				else if(rs.getString("role").equals("Manager")) {
-					emp = new Manager(rs.getString("eno"), rs.getString("secno"), rs.getString("name"), 
 							rs.getInt("enteryear"), rs.getInt("entermonth"), rs.getInt("enterday"));
 				}
 				resArray.add(emp);
@@ -173,18 +124,17 @@ public abstract class EmployeeDBIO extends ObjectDBIO implements EmployeeIO {
      * @return
      * @throws SQLException
      */
-    public ArrayList<Employee> selectEmp(String strENo) throws SQLException {
+    public ArrayList<Employee> searchEmployee(String strENo) {
         ArrayList<Employee> resArray = new ArrayList<Employee>();
         String selectSql = "select * from EMPLOYEE where eno = ?";
 
 		Connection conn = super.open();
-		PreparedStatement pstm = conn.prepareStatement(selectSql);
-		pstm.setString(1, strENo);	// secno
-
-
 		ResultSet rs = null;
 		try {
-			rs = super.execute(selectSql, rs);
+		    PreparedStatement pstm = conn.prepareStatement(selectSql);
+		    pstm.setString(1, strENo);	// secno
+
+            rs = pstm.executeQuery();
 			while(rs.next()) {
 				Employee emp = null;
 				if(rs.getString("role").equals("Staff")) {
@@ -197,37 +147,6 @@ public abstract class EmployeeDBIO extends ObjectDBIO implements EmployeeIO {
 				}
 				else if(rs.getString("role").equals("Manager")) {
 					emp = new Manager(rs.getString("eno"), rs.getString("secno"), rs.getString("name"),
-							rs.getInt("enteryear"), rs.getInt("entermonth"), rs.getInt("enterday"));
-				}
-				resArray.add(emp);
-			}
-			rs.close();
-			super.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return resArray;
-	}
-	
-	public ArrayList<Employee> searchEmployee(String strENo) {
-		ArrayList<Employee> resArray = new ArrayList<Employee>();
-		String strSql = "select * from EMPLOYEE where eno = '" + strENo + "'";
-		ResultSet rs = null;
-		
-		try {
-			rs = super.execute(strSql, rs);
-			while(rs.next()) {
-				Employee emp = null;
-				if(rs.getString("role").equals("Staff")) {
-					emp = new Staff(rs.getString("eno"), rs.getString("name"), rs.getInt("enteryear"), 
-							rs.getInt("entermonth"), rs.getInt("enterday"));
-				}
-				else if(rs.getString("role").equals("Secretary")) {
-					emp = new Secretary(rs.getString("eno"), rs.getString("name"), rs.getInt("enteryear"), 
-							rs.getInt("entermonth"), rs.getInt("enterday"));
-				}
-				else if(rs.getString("role").equals("Manager")) {
-					emp = new Manager(rs.getString("eno"), rs.getString("secno"), rs.getString("name"), 
 							rs.getInt("enteryear"), rs.getInt("entermonth"), rs.getInt("enterday"));
 				}
 				resArray.add(emp);
