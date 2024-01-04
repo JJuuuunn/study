7bfb1f339094e9ab9929cfb1ac08114082c7a863
jjuuuunn <didtdjwns111@gmail.com>
2024-01-04 12:01:58 +0900

    REFACTOR : Add AccessLevel Class
    직원들이 DB에 접근할 수 있는 권한을 주기 위해 테이블을 생성하였음으로 클래스 생성

---
+++ employee/AccessLevel.java
@@ -0,0 +1,10 @@
+package employee;
+
+import java.time.LocalDateTime;
+
+public class AccessLevel {
+    private Long id;
+    private String employeeId;
+    private LocalDateTime createdAt;
+    private LocalDateTime expiredAt;
+}
