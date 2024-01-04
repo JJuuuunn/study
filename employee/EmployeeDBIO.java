6a7fd468f40efa8eef5d8784bfae894eb19e7725
jjuuuunn <didtdjwns111@gmail.com>
2024-01-04 11:26:50 +0900

    DOCS : Add JavaDocs
    EmployeeDBIO 와 ObjectDBIO 메소드들에 주석(/**)을 사용하여 설명 추가

--- employee/EmployeeDBIO.java
+++ employee/EmployeeDBIO.java
@@ -10,10 +10,13 @@ import lib.ObjectDBIO;
 public abstract class EmployeeDBIO extends ObjectDBIO implements EmployeeIO {
 
     /**
-     * insertStaff 메소드
+     * 직원 정보를 추가하는 메소드
+     * secno는 매니저와 OnetoOne 관계이므로 여기서는 임시로 null
+     * <p>
+     * TODO : secno에 null 대신 넣을 값 생각하기
      *
      * @param emp
-     * @return
+     * @return boolean -> 정보가 등록되면 true
      */
     public boolean insertStaff(Staff emp) {
         String insertSql = "insert into EMPLOYEE values (?, ?, ?, ?, ?, ?, null)";
@@ -36,16 +39,23 @@ public abstract class EmployeeDBIO extends ObjectDBIO implements EmployeeIO {
         return true;
     }
 
+    /**
+     * Secretary 정보를 추가하는 메소드
+     * Staff 클래스가 Secretary 클래스의 부모 클래스이므로 호환 가능
+     *
+     * @param emp
+     * @return boolean -> 정보가 등록되면 true
+     */
     public boolean insertSecretary(Secretary emp) {
         return insertStaff(emp);
     }
 
     /**
-     * insertManager 메소드
+     * 매니저를 추가하는 메소드
+     * 최소 한명 이상의 Staff 또는 Secretary가 있어야 동작
      *
      * @param emp
-     * @return
-     * @throws SQLException
+     * @return boolean -> 정보가 등록되면 true
      */
     public boolean insertManager(Manager emp) {
         ArrayList<Employee> resArray = searchEmployee(emp.getSecNo());
@@ -75,7 +85,8 @@ public abstract class EmployeeDBIO extends ObjectDBIO implements EmployeeIO {
     }
 
     /**
-     * getEmployeeList 메소드
+     * 매니저를 제외한 전체 직원을 찾는 메소드
+     * TODO : 매니저를 제외하고 출력되도록 sql문 수정
      *
      * @param strUserID
      * @return EmployeeList
@@ -107,14 +118,15 @@ public abstract class EmployeeDBIO extends ObjectDBIO implements EmployeeIO {
     }
 
     /**
-     * searchEmployee 메소드
+     * 특정 직원을 찾는 메소드
+     * TODO : Role과 다른 데이터 접근 권한에 관한 테이블을 새로 만들어 추후에 권한을 학인 후 메소드 사용 가능하도록 변경
      *
      * @param strENo
      * @return EmployeeList
      * @throws SQLException
      */
     public ArrayList<Employee> searchEmployee(String strENo) {
-        ArrayList<Employee> resArray = new ArrayList<Employee>();
+        ArrayList<Employee> resArray = new ArrayList<>();
         String selectSql = "select * from EMPLOYEE where eno = ?";
 
         Connection conn = super.open();
@@ -138,8 +150,12 @@ public abstract class EmployeeDBIO extends ObjectDBIO implements EmployeeIO {
     }
 
     /**
+     * SQL을 통해서 데이터베이스에서 가져온 ResultSet을
+     * 각각의 Role에 맞게 Builder를 사용하여 Staff, Secretary, Manager로 변환.
+     * 생성자 패턴 Body에서 값을 넣던 Role, Firstpay, EnteringDay, NowPay를
+     * Builder에서 넣을 방법이 없어 따로 메소드를 사용하여 데이터를 넣음.
      *
-     * @param rs
+     * @param rs -> SQL 실행 결과값들이 담긴 Employee
      * @return Employee
      * @throws SQLException
      */
